4d82c56f693716b9a3af854a4aa3068b3b39def8
vsood <vsood@umd.edu>
2021-11-29 17:05:52 -0500

    [TASK 2] Added src files [avoid_walls.cpp] Added the obstacle avoidance logic in this file. [dvd_robot_node.cpp] spins a new node

---
+++ src/dvd_robot_node.cpp
@@ -0,0 +1,29 @@
+/**
+ *  @copyright MIT License, © 2021 Vivek Sood
+ *  @file    main.cpp
+ *  @author  Vivek Sood
+ *  @date    11/27/2021
+ *  @version 1.0
+ *  @brief   main file
+ */
+
+
+
+#include <ros/ros.h>
+#include <std_msgs/String.h>
+
+
+
+#include <dvd_robot/avoid_walls.hpp>
+
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "dvd_robot");
+  ros::NodeHandle nh;
+
+  ROS_INFO_STREAM("Starting walker bot node.... ");
+  std::unique_ptr<AvoidWalls> dvd_robot(new AvoidWalls(
+                                              nh));
+  ros::spin();
+  return 0;
+}
